607fe642c3f904fb3dd2df3ff4305ad3989dd007
Katie Wu <katiewu71@gmail.com>
2021-03-12 21:52:05 -0800

    adding composing, favoriting, and retweeting features

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -35,7 +35,9 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
-        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
     }
     
@@ -45,6 +47,8 @@ class HomeTableViewController: UITableViewController {
         
         myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
     }
     
     @objc func loadTweet() {
